e21ebb81d9ffbdae197f523eecb54aeeab2da70c
Meriem1551 <meriem200409@gmail.com>
2023-11-21 06:22:34 +0100

    Adding create function

--- how-to-create-a-tree/main.c
+++ how-to-create-a-tree/main.c
@@ -3,9 +3,15 @@
 /**
  * main - checks the code
  * Return: always 0
-*/
+ */
 
-int main (void)
+int main(void)
 {
-    
+    int num_nodes, data;
+    Tree *root;
+    printf("Enter number of nodes ");
+    scanf("%d", &num_nodes);
+    printf("What's the number for first node ");
+    scanf("%d", &data);
+    root = create_root(data);
 }
