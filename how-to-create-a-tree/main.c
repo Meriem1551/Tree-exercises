4b3c9c659c39ffba57f335cbb906c6d604a5327e
Meriem1551 <meriem200409@gmail.com>
2023-11-21 05:44:55 +0100

    initializing project

---
+++ how-to-create-a-tree/main.c
@@ -0,0 +1,11 @@
+#include "tree.h"
+
+/**
+ * main - checks the code
+ * Return: always 0
+*/
+
+int main (void)
+{
+    
+}
---
+++ how-to-create-a-tree/tree.h
@@ -0,0 +1,21 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tree Tree;
+/**
+ * tree - struct to create a tree
+ * @number: integer
+ * @right_child: pointer to a struct
+ * @left_child: pointer to a struct
+ */
+
+struct tree
+{
+    int number;
+    struct tree *right_child;
+    struct tree *left_child;
+};
+#endif
