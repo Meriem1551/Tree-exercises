063d3673e0c7114bbc18f8f5887ebcd170bc3633
Meriem1551 <meriem200409@gmail.com>
2023-11-23 05:44:32 +0100

    Changing text

--- how-to-create-a-tree/main.c
+++ how-to-create-a-tree/main.c
@@ -11,7 +11,13 @@ int main(void)
     Tree *root;
     printf("Enter number of nodes ");
     scanf("%d", &num_nodes);
-    printf("What's the number for first node ");
+    printf("What's the number for root ");
     scanf("%d", &data);
     root = create_root(data);
+    for (int i = 1; i < num_nodes; i++)
+    {
+        printf("What's the number for this child ");
+        scanf("%d", &data);
+        root = insert_child(root, data);
+    }
 }
