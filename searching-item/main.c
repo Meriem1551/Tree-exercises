c79afe8b3e79c027fd8e8e523c3338979cbb5528
Meriem1551 <meriem200409@gmail.com>
2023-11-24 10:09:27 +0100

    Initializing project

---
+++ searching-item/main.c
@@ -0,0 +1,10 @@
+#include "search.h"
+
+/**
+ * main - checks code
+ * Rreturn: always 0
+ */
+
+int main(void)
+{
+}
---
+++ searching-item/search.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../how-to-create-a-tree/tree.h"
+
+#endif
