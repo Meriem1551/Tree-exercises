9dedd3246d27cb1f1667639ac2818bb1c91e6268
Meriem1551 <meriem200409@gmail.com>
2023-11-25 10:15:09 +0100

    Fixing some errors

--- searching-item/search.h
+++ searching-item/search.h
@@ -1,12 +1,10 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "../how-to-create-a-tree/tree.h"
-#include <stdbool.h>
-
 bool isExiste(Tree *, int);
-
 void print_message(char *);
 #endif
